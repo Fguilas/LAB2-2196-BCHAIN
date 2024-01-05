1284f5066ec009d6f248de7bf640fb009d3a629a
Felino Guilas <128701336+Fguilas@users.noreply.github.com>
2024-01-05 15:29:43 +0800

    Add files via upload

---
+++ Booklist.jsx
@@ -0,0 +1,10 @@
+export default function Booklist() {
+    return(
+        <div>
+            <h2>Book List</h2>
+            <li>Rich Dad Poor Dad</li>
+            <li>Diary of Pulubi</li>
+            <li>Wimpy Kid</li>
+        </div>
+    );
+}
---
+++ MovieList.jsx
@@ -0,0 +1,10 @@
+export default function MovieList() {
+    return(
+        <div>
+            <h2>Movie List</h2>
+            <li>Home Alone 1</li>
+            <li>Home Alone 2</li>
+            <li>Home Alone 3</li>
+        </div>
+    );
+}
